3b6dfc55c9fbc3649217c7c8e4311692e81edb59
Avihay Menahem <avihay@three-dev.com>
2014-12-31 00:25:58 +0200

    Added loading progress

--- app/shared/player/playerController.js
+++ app/shared/player/playerController.js
@@ -1,87 +1,54 @@
-app.controller('playerController', function ($scope) {
-    var ytdl = require('ytdl'),
-        request = require('request'),
-        moment = require('moment');
-
-    $scope.currentPlayingTrack = {
-        title : "No Track Selected",
-        artist : "No Track Selected",
-        cover_url_medium : "",
-        cover_url_large : ""
-    };
-
-    $scope.currentProgress = "50%";
-    $scope.currentHash = null;
-    $scope.currentPlayingTrackString = $scope.currentPlayingTrack.artist + " - " + $scope.currentPlayingTrack.title;
-
-    videojs('video_player', {
-        controls : false,
-        preload : "auto"
-    });
-
-    $scope.$on('trackChangedEvent', function(event, args){
-        $scope.currentPlayingTrack = args.trackObject;
-        $scope.currentHash = $scope.getHash(args.trackObject);
-
-        $scope.getVideo({
-            url : 'http://gdata.youtube.com/feeds/api/videos?alt=json&max-results=1&q=' + encodeURIComponent(args.trackObject.artist + ' - ' + args.trackObject.title),
-            json : true
-        }, function (error, response, data) {
-            if(!data.feed.entry)
-            {
-                console.log("Not Found");
-            }
-            else
-            {
-                $scope.getInfo(data.feed.entry[0].link[0].href, { downloadURL : true }, function (err, info) {
-                    if(err)
-                    {
-                        console.log(err);
-                    }
-                    else
-                    {
-                        var streamUrls = [];
-                        var itag_priorities = [85,43,82];
-
-                        for(var f in info.formats)
-                        {
-                            var currentFormat = info.formats[f];
-                            streamUrls[currentFormat.itag] = currentFormat.url;
-                        }
-
-                        for(var it in itag_priorities)
-                        {
-                            if(streamUrls[itag_priorities[it]])
-                            {
-                                videojs('video_player').src(streamUrls[itag_priorities[it]]).play();
-                            }
-                        }
-                    }
-                })
-            }
-        })
-    });
-
-    videojs('video_player').ready(function(){
-        this.on('loadedmetadata', function () {
-
-        });
-
-        this.on('timeupdate', function () {
-            $scope.currentProgress = (this.currentTime() / this.duration() * 100) + "%";
-            $scope.$apply();
-        });
-    });
-
-    $scope.getHash = function(trackObject) {
-        return trackObject.title + "-" + trackObject.artist;
-    };
-
-    $scope.getVideo = function(options, cb){
-        request(options, cb);
-    };
-    
-    $scope.getInfo = function (link, options, cb) {
-        ytdl.getInfo(link, options, cb);
+app.factory('playerService', function () {
+    return {
+        element : "video_player",
+        isPlaying : false,
+        ref : null,
+        init: function(){
+            this.ref = videojs(this.element, {
+                controls : false,
+                preload : "auto"
+            });
+            return this;
+        },
+        stop : function () {
+            this.ref.stop();
+            return this;
+        },
+        play : function () {
+            this.isPlaying = true;
+            this.ref.play();
+            return this;
+        },
+        pause : function () {
+            this.ref.pause();
+            return this;
+        },
+        togglePlay : function(){
+            this.isPlaying ? this.play() : this.pause();
+            this.isPlaying = !this.isPlaying;
+            return this;
+        },
+        setSrc : function (src) {
+            this.ref.src(src);
+            return this;
+        },
+        playSource : function(src)
+        {
+            this.ref.src(src).play();
+            return this;
+        },
+        ready : function(obj) {
+            this.ref.ready(obj);
+            return this;
+        },
+        goToDuration : function (event) {
+            var elementWidth = event.target.offsetWidth,
+                clickOffsetX = event.offsetX,
+                percent = (clickOffsetX / elementWidth) * this.ref.duration();
+            this.ref.currentTime(percent);
+        },
+        getLoadingProgress : function() {
+            return this.ref.bufferedPercent() * 100;
+        }
     };
 });
